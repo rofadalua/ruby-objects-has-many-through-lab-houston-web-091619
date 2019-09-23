a50eaa482edbd712f1e58b0bdb9841209093fc1b
fabiola mendoza <rofadalua@hotmail.com>
2019-09-23 05:24:25 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,10 @@
 class Appointment
   attr_accessor :date, :doctor, :patient
 
-  def initialize(date, doctor)
+  def initialize(date, doctor, patient)
     @date = date
     @doctor = doctor
+    @patient = patient 
     doctor.add_appointment(self)
   end
 
